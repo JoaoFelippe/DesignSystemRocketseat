fd4ddcf18ec4a9b96426ef7571d4c9367722d56a
JoaoFelppe <j.felippe.s.castro@hotmail.com>
2025-05-29 22:26:00 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -29,5 +29,12 @@ const config: StorybookConfig = {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
+  viteFinal: (config, {configType}) => {
+    if (configType === 'PRODUCTION'){
+      config.base = '/DesignSystemRocketseat'
+    }
+
+    return config
+  }
 };
 export default config;
